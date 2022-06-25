51a97211125205c5578a73caded582628bb96c7d
ProgRs <lfernandotc@hotmail.com>
2022-06-25 14:51:01 -0300

    Finalização do projeto

--- app/src/main/java/com/br/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/br/gastoviagem/MainActivity.kt
@@ -2,9 +2,11 @@ package com.br.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.br.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +15,48 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.buttonCalculate.setOnClickListener(this)
 
-        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        //var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
     }
 
-    private fun calculate(){
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_calculate){
+            calculete()
+        }
+    }
+
+    private fun isValid(): Boolean{
+         return (binding.editDistance.text.toString() != ""
+                 && binding.editPrice.text.toString() !== ""
+                 && binding.editAutonomy.text.toString() != ""
+                 && binding.editAutonomy.text.toString().toFloat() != 0f
+                 )
+
 
     }
+
+    private fun calculete(){
+            //Toast notification
+
+        if(isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+            binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        }else{
+            Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_SHORT).show()
+        }
+
+        //Toast.makeText(this, totalValueStr, Toast.LENGTH_SHORT).show()
+
+
+    }
+
+
+
+
 }
